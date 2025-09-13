64ec1a6a588bcedd661e2a9dcbccace56875d3a2
Ikejiri-1 <pedroike21@gmail.com>
2025-09-12 23:36:32 -0300

    edição dos cards

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,32 @@ import "./Main.css";
 import { ProceduresTags } from "./ProceduresTags";
 import { Cards } from "../../components/Cards";
 import { Form } from "../../components/Form";
+import cardImg from "/src/assets/img/ozonioterapia.png";
+
+const cardsContent = [
+  {
+    id: 1,
+    image: cardImg,
+    title: "Uma aliada poderosa no cuidado da sua saúde",
+    text: "Você já ouviu falar da ozonioterapia, mas ainda tem dúvidas sobre como ela funciona e de que forma pode contribuir para o tratamento de doenças crônicas? A ozonioterapia é uma técnica terapêutica que utiliza uma mistura de oxigênio e ozônio medicinal, aplicada de forma controlada, com o objetivo de estimular o equilíbrio e a regeneração do organismo. Com propriedades anti-inflamatórias, antioxidantes, antimicrobianas e imunomoduladoras, o ozônio tem sido utilizado há décadas em países da Europa e América Latina com resultados comprovados por diversos estudos científicos.",
+  },
+  {
+    id: 2,
+    title: "Tratamento de doenças:",
+    text: "A ozonioterapia tem se mostrado eficaz no tratamento de doenças como artrite, artrose, hérnia de disco, fibromialgia, asma, doenças cardiovasculares, diabetes e até mesmo como coadjuvante em tratamentos oncológicos. Também é uma aliada em processos de emagrecimento, cicatrização, rejuvenescimento estético e, mais recentemente, em protocolos de reabilitação de dependência química, auxiliando na redução dos sintomas de abstinência.",
+  },
+  {
+    id: 3,
+    title: "Tratamento Seguro e Personalizado:",
+    text: "Apesar de seus inúmeros benefícios, é importante lembrar que cada organismo é único. Por isso, o tratamento com ozonioterapia deve ser individualizado, respeitando as necessidades e condições específicas de cada paciente. Se você busca uma alternativa segura, natural e cientificamente respaldada para melhorar sua saúde, a ozonioterapia pode ser o caminho ideal para o seu bem-estar físico, emocional e social.",
+  },
+  {
+    id: 4,
+    title: "Benefícios Comprovados:",
+    text: "Fortalecimento do sistema imunológico\n Alívio de dores crônicas\n Melhora da circulação sanguínea e oxigenação dos tecidos\n Auxílio no controle da glicemia em pacientes diabéticos\n Ação anti-inflamatória e analgésica natural\n Redução do estresse oxidativo e combate aos radicais livres",
+    className: "multiline-text",
+  },
+];
 
 export function Home() {
   return (
@@ -28,7 +54,7 @@ export function Home() {
         </div>
 
         <div className="cards-container">
-          <Cards></Cards>
+          <Cards cards={cardsContent}></Cards>
         </div>
         <div className="form-container">
           <Form></Form>
